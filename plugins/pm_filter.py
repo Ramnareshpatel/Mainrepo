e64a3885116b3f4046cd3d3c84b1fa5f5d1822e2
Ramnareshpatel <95334027+Ramnareshpatel@users.noreply.github.com>
2022-06-14 09:19:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -199,27 +199,27 @@ async def next_page(bot, query):
         ]
     )
 
-    if 0 < offset <= 6:
+    if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
         off_set = None
     else:
-        off_set = offset - 6
+        off_set = offset - 10
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton("◁ ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"📜 ᴘᴀɢᴇ {round(int(offset) / 6) + 1} / {round(total / 6)}",
+             InlineKeyboardButton(f"📜 ᴘᴀɢᴇ {round(int(offset) / 10) + 1} / {round(total / 10)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton(f"📜 ᴘᴀɢᴇ {round(int(offset) / 6) + 1} / {round(total / 6)}", callback_data="pages"),
+            [InlineKeyboardButton(f"📜 ᴘᴀɢᴇ {round(int(offset) / 10) + 1} / {round(total / 6)}", callback_data="pages"),
              InlineKeyboardButton("ɴᴇxᴛ ▷", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
                 InlineKeyboardButton("◁ ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"📜 ᴘᴀɢᴇ {round(int(offset) / 6) + 1} / {round(total / 6)}", callback_data="pages"),
+                InlineKeyboardButton(f"📜 ᴘᴀɢᴇ {round(int(offset) / 10) + 1} / {round(total / 6)}", callback_data="pages"),
                 InlineKeyboardButton("ɴᴇxᴛ ▷ ", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
@@ -1409,7 +1409,7 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"📜 ᴘᴀɢᴇ 1/{round(int(total_results) / 6)}", callback_data="pages"),
+            [InlineKeyboardButton(text=f"📜 ᴘᴀɢᴇ 1/{round(int(total_results) / 10)}", callback_data="pages"),
              InlineKeyboardButton(text="ɴᴇxᴛ ▷", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
